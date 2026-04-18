cb96c93da7e62d5d28154ec6c5b0a3f2a4567073
shrawanii15 <shrawanipawar10806@gmail.com>
2026-04-18 22:25:56 +0530

    Producer Consumer.c

---
+++ Producer Consumer.c
@@ -0,0 +1,88 @@
+//To apply inter-process communication mechanisms and synchronize processes by implementing solution to classical synchronization problem.
+
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+#define SIZE 5
+
+int buffer[SIZE];
+int in = 0, out = 0;
+
+sem_t empty, full;
+pthread_mutex_t mutex;
+
+void* producer(void* arg)
+{
+    int item;
+    for(int i = 0; i < 5; i++)
+    {
+        item = i + 1;
+
+        sem_wait(&empty);
+        pthread_mutex_lock(&mutex);
+
+        buffer[in] = item;
+        printf("Produced: %d\n", item);
+        in = (in + 1) % SIZE;
+
+        pthread_mutex_unlock(&mutex);
+        sem_post(&full);
+
+        sleep(1);
+    }
+}
+
+void* consumer(void* arg)
+{
+    int item;
+    for(int i = 0; i < 5; i++)
+    {
+        sem_wait(&full);
+        pthread_mutex_lock(&mutex);
+
+        item = buffer[out];
+        printf("Consumed: %d\n", item);
+        out = (out + 1) % SIZE;
+
+        pthread_mutex_unlock(&mutex);
+        sem_post(&empty);
+
+        sleep(2);
+    }
+}
+
+int main()
+{
+    pthread_t p, c;
+
+    sem_init(&empty, 0, SIZE);
+    sem_init(&full, 0, 0);
+    pthread_mutex_init(&mutex, NULL);
+
+    pthread_create(&p, NULL, producer, NULL);
+    pthread_create(&c, NULL, consumer, NULL);
+
+    pthread_join(p, NULL);
+    pthread_join(c, NULL);
+
+    sem_destroy(&empty);
+    sem_destroy(&full);
+    pthread_mutex_destroy(&mutex);
+
+    return 0;
+}
+
+/*Output -
+
+Produced: 1
+Consumed: 1
+Produced: 2
+Produced: 3
+Consumed: 2
+Produced: 4
+Consumed: 3
+Produced: 5
+Consumed: 4
+Consumed: 5 */
